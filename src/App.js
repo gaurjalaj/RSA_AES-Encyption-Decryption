21d6a2d83157e1e692be3b4fe7fac609ae97ea7c
Jalaj Kumar Gaur <105718576+gaurjalaj@users.noreply.github.com>
2022-11-07 23:05:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,27 +5,8 @@ import AES from "./components/AES"
 function App() {
 
   const [state, setstate] = useState({
-    // key : '',
-    // text : '',
-    // encryption_result : "",
-    // decryption_result : "",
     selectedAlgorithm: ""
   });
-
-
-  // const encrypt = () => {
-  //   console.log(state);
-  //   const rsaEncrypted = rsaEncryption(state.key, state.text)
-  //   setstate({...state, encryption_result : rsaEncrypted, decryption_result : ""});
-  //   console.log(rsaEncrypted);
-  // }
-
-  
-  // const decrypt = () => {
-  //   console.log(state);
-  //   const rsaDecrypted = rsaDecryption(state.key, state.text);
-  //   setstate({...state, encryption_result : "", decryption_result : rsaDecrypted});
-  // }
 
 
   return (
@@ -38,13 +19,6 @@ function App() {
       {
         state.selectedAlgorithm==="RSA" ? <RSA /> : (state.selectedAlgorithm==="AES" ? <AES /> : '')
       }
-      {/* <p>Enter text:</p>
-      <textarea onChange = {(ev) => setstate({...state, text: ev.target.value})} type="text" name="text" />&nbsp;
-      <p>Enter Public/privateKey</p>
-      <textarea onChange = {(ev) => setstate({...state, key: ev.target.value})} type="text" name="key" /><br /><br />
-      <button onClick={() => encrypt()} id='enc'>Encrypt</button>&nbsp;
-      <button onClick={() => decrypt()} id='dec'>Decrypt</button>
-      <div id='output'><h1>Output::</h1>{state.encryption_result ? `encryption_result:: ${state.encryption_result}` : state.decryption_result ? (`decryption_result:: ${state.decryption_result}`) : ""}</div> */}
       </div>
 
   )
